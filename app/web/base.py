a7333518cc53b8e5f8fe858a2dd3e5b5e669c690
Artemiy Solopov <art-solopov@yandex.ru>
2017-08-13 12:17:59 +0300

    Templating

--- app/web/base.py
+++ app/web/base.py
@@ -1,7 +1,9 @@
 from bottle import Bottle
 
+from .templating import render
+
 base_app = Bottle()
 
 @base_app.route('/')
 def home():
-    return 'Hello, world!'
+    return render('home.html')
---
+++ app/web/templating.py
@@ -0,0 +1,11 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('templates'),
+    autoescape=select_autoescape(['html'])
+)
+
+def render(template, options=None):
+    tmpl = env.get_template(template)
+    options = options or {}
+    return tmpl.render(**options)
