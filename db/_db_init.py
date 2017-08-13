03ea9679c577d3c8518ece8736b57f1e7057fe97
Artemiy Solopov <art-solopov@yandex.ru>
2017-08-13 11:18:14 +0300

    Some more models changes

--- db/_db_init.py
+++ db/_db_init.py
@@ -6,9 +6,12 @@ from ._base import models_registry as registry
 storage = ZODB.FileStorage.FileStorage('data.fs')
 db = ZODB.DB(storage)
 
-def db_init():
-    with db.transaction() as conn:
-        root = conn.root
-        for model in registry:
-            if not hasattr(root, model.model_name):
-                setattr(root, model.model_name, OOBTree())
+def ensure_index(index_name, transaction=None):
+    if transaction:
+        root = transaction.root
+        if not hasattr(root, index_name):
+            setattr(root, index_name, OOBTree())
+        return getattr(root, index_name)
+    else:
+        with db.transaction() as conn:
+            return ensure_index(index_name, conn)
--- db/models.py
+++ db/models.py
@@ -1,6 +1,10 @@
 import persistent.list as plist
+from BTrees.OOBTree import OOBTree
 
 from ._base import Base
+
+class IdExistError(Exception):
+    pass
 
 class Book(Base):
     def __init__(self, authors=None, publisher=None, year=None, title=''):
@@ -8,13 +12,27 @@ class Book(Base):
         self.publisher = publisher
         self.year = year
         self.title = title
+        self._id = None
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        if self._id is None:
+            self._id = value
+        else:
+            raise IdExistError()
 
 
 class Author(Base):
     def __init__(self, name=''):
         self.name = name
+        self.books = OOBTree()
 
 
 class Publisher(Base):
     def __init__(self, name=''):
         self.name = name
+        self.books = OOBTree()
